bfb8eca6345cbf1f62beca18b674b2d74cf6837d
cmruss <cmrussell08@gmail.com>
2020-04-28 14:36:43 -0400

    created api, able to add PersonalNotes via app

--- notes/api.py
+++ notes/api.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers, viewsets
-from notes import PersonalNote
+from .models import PersonalNote
 
 class PersonalNoteSerializer(serializers.HyperlinkedModelSerializer):
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+        note = PersonalNote.objects.create(user=user,**validated_data)
+        return note
+       
+
     class Meta:
         model = PersonalNote
         fields = ('title', 'content')
